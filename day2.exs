295aec501db3625b0add11afd591bbacf2ab0771
Orga Shih <orga.shih@gmail.com>
2018-12-02 20:20:54 +0800

    day 2 part 2 elixir

--- day2.exs
+++ day2.exs
@@ -18,6 +18,24 @@ defmodule Day2 do
     checksum(inputs, twice_num + twice_exist, thrice_num + thrice_exist)
   end
 
+  def common_letter([_|js]=is) do
+    common_letter(is, js, false)
+  end
+  def common_letter([], [], common), do: common
+  def common_letter([_|[_|js]=is], [], false), do: common_letter(is, js, false)
+  def common_letter([i|_]=is, [j|js], false), do: common_letter(is, js, correct_box_id(i, j))
+  def common_letter(_, _, common), do: common
+
+  defp correct_box_id(id1, id2, common \\ "", diff \\ 0)
+  defp correct_box_id(_, _, _, 2), do: false
+  defp correct_box_id("", "", common, 1), do: common
+  defp correct_box_id(<<c1::size(8), id1::binary>>, <<c1::size(8), id2::binary>>, common, diff) do
+    correct_box_id(id1, id2, <<common::binary, c1>>, diff)
+  end
+  defp correct_box_id(<<_::size(8), id1::binary>>, <<_::size(8), id2::binary>>, common, diff) do
+    correct_box_id(id1, id2, common, diff + 1)
+  end
+
   def load_input do
     File.read!(@input_path)
     |> String.split("\n")
@@ -46,6 +64,21 @@ defmodule Day2Test do
     assert 12 === Day2.checksum(inputs)
 
     assert 5681 === Day2.load_input() |> Day2.checksum()
+  end
+
+  test "common letter correct" do
+    inputs = [
+      "abcde",
+      "fghij",
+      "klmno",
+      "pqrst",
+      "fguij",
+      "axcye",
+      "wvxyz"
+    ]
+
+    assert "fgij" == Day2.common_letter(inputs)
+    assert "uqyoeizfvmbistpkgnocjtwld" == Day2.load_input() |> Day2.common_letter()
   end
 
 end
